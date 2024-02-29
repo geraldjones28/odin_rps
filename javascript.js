7c06f51b544237eaac8395b2fca8fcea1589195c
GeraldJones <gajone12@asu.edu>
2024-02-29 04:42:13 -0800

    feat: Complete oneRound function to be able to play one round of RPS

--- javascript.js
+++ javascript.js
@@ -10,10 +10,24 @@ function getComputerChoice() {
     }
 }
 
-/* function oneRound(playerChoice, comp = getComputerChoice()) {
-    if (playerChoice === comp) {
+function oneRound(playerChoice, comp) {
+    if (playerChoice.toUpperCase() === comp.toUpperCase()) {
         return "Tie!";
+    } else if (playerChoice.toUpperCase() === "ROCK" && comp.toUpperCase() === "SCISSORS") {
+        return "Player wins! Rock beats scissors!";
+    } else if (comp.toUpperCase() === "ROCK" && playerChoice.toUpperCase() === "SCISSORS") {
+        return "Computer wins! Rock beats scissors!";
+    } else if (playerChoice.toUpperCase() === "SCISSORS" && comp.toUpperCase() === "PAPER") {
+        return "Player wins! Scissors beats paper!";
+    } else if (comp.toUpperCase() === "SCISSORS" && playerChoice.toUpperCase() === "PAPER") {
+        return "Computer wins! Scissors beats paper!";
+    } else if (playerChoice.toUpperCase() === "PAPER" && comp.toUpperCase() === "ROCK") {
+        return "Player wins! Paper beats rock!";
+    } else if (comp.toUpperCase() === "PAPER" && playerChoice.toUpperCase() === "ROCK") {
+        return "Computer wins! Paper beats rock!";
+    } else {
+        return "null";
     }
-} */
+}
 
-console.log(getComputerChoice());
+console.log(oneRound("rock", getComputerChoice()));

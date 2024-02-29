966137cda81aeb7c7659cc4b5e3a67edc49f1d16
GeraldJones <gajone12@asu.edu>
2024-02-28 19:27:44 -0800

    fix: Implement non-case sensitive answers for getComputerChoice function

--- javascript.js
+++ javascript.js
@@ -1,15 +1,22 @@
-console.log("Hello, world!");
-
 function getComputerChoice() {
     let num = Math.floor(Math.random() * 30) + 1;
     
     if (num <= 10) {
-        return "Rock";
+        comp = "rock";
+        return comp.toUpperCase();
     } else if (num > 10 && num <= 20) {
-        return "Scissors";
+        comp = "paper";
+        return comp.toUpperCase();
     } else {
-        return "Paper";
+        comp = "scissors";
+        return comp.toUpperCase();
     }
 }
+
+/* function oneRound(playerChoice, comp = getComputerChoice()) {
+    if (playerChoice === comp) {
+        return "Tie!";
+    }
+} */
 
 console.log(getComputerChoice());

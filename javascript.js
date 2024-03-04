004e3f88c6afa26659013df4b1ed021c3df0190c
GeraldJones <gajone12@asu.edu>
2024-03-04 07:59:22 -0800

    feat: Rounds properly display winning results for player,computer, and tie.

--- javascript.js
+++ javascript.js
@@ -3,9 +3,9 @@ let playerScore = 0;
 let computerScore = 0;
 
 /* Buttons */
-const rockBtn = document.querySelector('.option-rock');
-const paperBtn = document.querySelector('.option-paper');
-const scissorsBtn = document.querySelector('.option-scissors');
+const rockBtn = document.querySelector('#option-rock');
+const paperBtn = document.querySelector('#option-paper');
+const scissorsBtn = document.querySelector('#option-scissors');
 
 /* Button Event Listeners */
 rockBtn.addEventListener('click', () => {
@@ -18,6 +18,12 @@ scissorsBtn.addEventListener('click', () => {
     playRound('scissors', getComputerChoice());
 });
 
+/* Round Results */
+let roundPlayer = document.querySelector('#round-player');
+let roundComp = document.querySelector('#round-comp');
+let roundTie = document.querySelector('#round-tie');
+
+/* Randomizes computer choice */
 function getComputerChoice() {
     let num = Math.floor(Math.random() * 3); + 1
     
@@ -30,40 +36,67 @@ function getComputerChoice() {
     }
 }
 
+/* Simulates a round of RPS based on which button user clicks */
 function playRound(playerChoice, comp) {
+    /* if-else game results branches */
     if (playerChoice.toUpperCase() === "ROCK") {
         if (comp === "SCISSORS") {
             playerScore++;
+            roundPlayer.innerHTML = 'PLAYER WINS';
+            setTimeout(clearFunction, 1500);
         } else if (comp === 'PAPER') {
             computerScore++;
+            roundComp.innerHTML = 'COMPUTER WINS';
+            setTimeout(clearFunction, 1500);
         } else {
             console.log('tie');
+            roundTie.innerHTML = 'TIE';
+            setTimeout(clearFunction, 1500);
         }
         console.log('player:[' + playerChoice + '] score:[' + 
         playerScore + ']' + 'comp:[' + comp + '] score:[' + computerScore + ']');
     } else if (playerChoice.toUpperCase() === "SCISSORS") {
         if (comp === "PAPER") {
             playerScore++;
+            roundPlayer.innerHTML = 'PLAYER WINS';
+            setTimeout(clearFunction, 1500);
         } else if (comp === 'ROCK') {
             computerScore++;
+            roundComp.innerHTML = 'COMPUTER WINS';
+            setTimeout(clearFunction, 1500);
         } else {
             console.log('tie');
+            roundTie.innerHTML = 'TIE';
+            setTimeout(clearFunction, 1500);
         }
         console.log('player:[' + playerChoice + '] score:[' + 
         playerScore + ']' + 'comp:[' + comp + '] score:[' + computerScore + ']');
     } else if (playerChoice.toUpperCase() === "PAPER") {
         if (comp === "ROCK") {
             playerScore++;
+            roundPlayer.innerHTML = 'PLAYER WINS';
+            setTimeout(clearFunction, 1500);
         } else if (comp === 'SCISSORS') {
             computerScore++;
+            roundComp.innerHTML = 'COMPUTER WINS';
+            setTimeout(clearFunction, 1500);
         } else {
             console.log('tie');
+            roundTie.innerHTML = 'TIE';
+            setTimeout(clearFunction, 1500);
         }
         console.log('player:[' + playerChoice + '] score:[' + 
         playerScore + ']' + 'comp:[' + comp + '] score:[' + computerScore + ']');
     } else {
         console.log('deadgame');
     }
+}
+
+/* Clears the innerHTML of round results after 1.5 seconds */
+function clearFunction() {
+    roundPlayer.innerHTML = '';
+    roundComp.innerHTML = '';
+    roundTie.innerHTML = '';
 }
 
 function consoleCheck() {
